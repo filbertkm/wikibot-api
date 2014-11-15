d0ebfb88c515193b753381398b205c9562ec96d8
aude <aude.wiki@gmail.com>
2014-11-15 17:52:25 +0100

    implement PropertyDataTypeApiLookup

--- src/Wikibase/PropertyDataTypeApiLookup.php
+++ src/Wikibase/PropertyDataTypeApiLookup.php
@@ -4,11 +4,17 @@ namespace Wikibot\Api\Wikibase;
 
 use Wikibase\DataModel\Entity\PropertyDataTypeLookup;
 use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\Store\EntityLookup;
 
 class PropertyDataTypeApiLookup implements PropertyDataTypeLookup {
 
-	public function getDataTypeIdForProperty( PropertyId $propertyId ) {
+	public function __construct( EntityLookup $entityLookup ) {
+		$this->entityLookup = $entityLookup;
+	}
 
+	public function getDataTypeIdForProperty( PropertyId $propertyId ) {
+		$property = $this->entityLookup->getEntity( $propertyId );
+		return $property->getDataTypeId();
 	}
 
 }
