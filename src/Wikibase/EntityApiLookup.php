abc75d7d6ce4fde26385cf07d30110af909804db
aude <aude.wiki@gmail.com>
2014-11-15 15:20:16 +0100

    add dependency on wikibase-store

---
+++ src/Wikibase/EntityApiLookup.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wikibot\Api\Wikibase;
+
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Store\EntityLookup;
+
+class EntityApiLookup implements EntityLookup {
+
+	public function getEntity( EntityId $entityId ) {
+
+	}
+
+	public function hasEntity( EntityId $entityId ) {
+
+	}
+
+}
---
+++ src/Wikibase/PropertyDataTypeApiLookup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wikibot\Api\Wikibase;
+
+use Wikibase\DataModel\Entity\PropertyDataTypeLookup;
+use Wikibase\DataModel\Entity\PropertyId;
+
+class PropertyDataTypeApiLookup implements PropertyDataTypeLookup {
+
+	public function getDataTypeIdForProperty( PropertyId $propertyId ) {
+
+	}
+
+}
